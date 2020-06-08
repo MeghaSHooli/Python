02680ec92d8e1ff50dc894dd78b5fbe5c7e8ae0a
mhooli <62090550+MeghaSHooli@users.noreply.github.com>
2020-06-08 12:18:06 +0530

    Demonstration of basics of Python to give KT1

---
+++ Basics.py
@@ -0,0 +1,56 @@
+"""
+Demonstration of basics of Python to give KT
+
+"""
+# Created by: mhooli
+# Version -V0.1
+
+#Basic of Print
+
+print("Welcome to Megha Demo1")
+
+print(111) #INT
+print(111.1111) #Folat
+print("----")
+print(int(111.111)) #converting to int
+print(float(6756))#converting to float
+print(+88)
+print(-111.8888)
+
+print("----------------")
+
+
+#Simple Arithmetic
+print("Arithmetic")
+print(1 + 5)
+
+print(5558 - 5555)
+
+print(569 * 55)
+
+print(16 / 8) # Gives the floating output
+print(16 // 8) # Gives the int output
+
+print(8 ** 2) #Exponentials 8^2
+
+print(3+5+7+9) # left to right open
+
+print(((18+67) / 9 )* 6) # Compound exp
+
+#Sample on variables
+temperature = 95
+bakers_dozen = 12+5
+
+# Variables can be used as values and in expressions
+
+print(temperature, bakers_dozen)
+print("celsius:", (temperature - 32) * 5 / 9)
+print("fahrenheit:", float(temperature))
+
+
+
+
+
+
+
+
